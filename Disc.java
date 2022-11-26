12a332e0cb04bf94a01dec5a48b1df915a11ec25
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-11-26 00:15:19 -0800

    Add files via upload

---
+++ Disc.java
@@ -0,0 +1,17 @@
+package plot_four;
+
+import game.Monad;
+import javafx.scene.Node;
+
+public class Disc extends Monad
+{
+	public Disc(Node icon, String name, double value) {
+		super(icon, name, value);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean move() 
+	{
+		return false;
+	}
+}
