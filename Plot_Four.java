a16576d2793cfcf7b27427351a5e218a2cc3ca78
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-11-26 00:16:46 -0800

    Update Plot_Four.java

--- Plot_Four.java
+++ Plot_Four.java
@@ -1,5 +1,29 @@
 package plot_four;
 
-public class Plot_Four {
+import boardgame.Boardgame;
+import boardgame.Unit;
+import game.Monad;
+import game.Grid;
+import boardgame.Position;
 
+public class Plot_Four extends Boardgame
+{
+	protected void setGrid(int rows, int cols) 
+	{
+		Position[][] positions = new Position[rows][cols];
+		grid = new Grid(positions);
+		
+		for(int row = 0; row < rows; row++) 
+		{
+			for(int col = 0; col < cols; col++) 
+			{
+				positions[row][col] = new Position(null, "", 0, null);
+			}
+		}
+	}
+	
+	protected void setUnits(int rows, int cols) 
+	{
+		
+	}
 }
