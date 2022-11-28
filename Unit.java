c28efae425c4886b478b7bf60d4f39afa52fa812
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-11-28 00:51:17 -0800

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,1 +1,13 @@
+package boardgame;
 
+import game.Monad;
+import javafx.scene.Node;
+
+public abstract class Unit extends Monad {
+	public Unit(Node icon, String name, double value) {
+		super(icon, name, value);
+		// TODO Auto-generated constructor stub
+	}
+
+	public abstract boolean move(Position[][] grid, Position pos);
+}
