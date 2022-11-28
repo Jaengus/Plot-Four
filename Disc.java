dad2676b6f92fdcedd3fa30a6d552ec34a6d0413
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-11-28 00:51:51 -0800

    Add files via upload

--- Disc.java
+++ Disc.java
@@ -1,17 +1,39 @@
 package plot_four;
 
-import game.Monad;
 import javafx.scene.Node;
+import boardgame.Unit;
 
-public class Disc extends Monad
+public class Disc extends Unit
 {
 	public Disc(Node icon, String name, double value) {
 		super(icon, name, value);
-		// TODO Auto-generated constructor stub
 	}
 
-	public boolean move() 
-	{
-		return false;
+	public boolean move(Position[][] grid, Position pos) {
+		if (grid[pos.row][pos.col].unit == null) {
+
+			//Drops down the rows before plotting a piece (if the column is open)
+			while (pos.row < grid.length && grid[pos.row][pos.col].unit == null)
+				pos.row++;
+			
+			//Plots the unit in the matrix
+			grid[--pos.row][pos.col].unit = this; 
+
+			//Checks to see if there is a victor or if there are no more moves left
+			if (win(grid, pos, -1, 0, 0) || ++n >= 42) {
+				return false;
+			}
+			
+			//Changes whose turn it is
+			turn = (turn < 1) ? 1 : 0; 
+
+			//Returns that there are moves to make
+			return true;
+		}
+		else if (n < 42)
+			return true; //Column is empty but there are still moves to make 
+		else
+			return false; // No more moves to make
 	}
+}
 }
