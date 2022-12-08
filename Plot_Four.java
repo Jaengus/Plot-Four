a97cad40caa8fa944d22c86984c3f660200128be
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-12-08 15:40:21 -0800

    Update Plot_Four.java

--- Plot_Four.java
+++ Plot_Four.java
@@ -1,25 +1,33 @@
 package plot_four;
 
+import javafx.scene.image.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import game.Monad;
-import game.Grid;
+import game.Position;
+import game.Position_Manager;
+import game.Unit_Manager;
 import boardgame.Boardgame;
-import boardgame.Unit;
-import boardgame.Position;
 import plot_four.Disc;
 
 public class Plot_Four extends Boardgame
 {
+	public Plot_Four(String title, String serial, double price, boolean purchased) 
+	{
+		super(title, serial, price, purchased);
+	}
+
 	@Override
 	protected void setGrid(int rows, int cols) 
 	{
 		Position[][] positions = new Position[rows][cols];
-		grid = new Grid(positions);
+		position_Manager = new Position_Manager(positions);
 		
 		for(int row = 0; row < rows; row++) 
 		{
 			for(int col = 0; col < cols; col++) 
 			{
-				positions[row][col] = new Position(null, "", 0, null);
+				positions[row][col] = new Position(null, null, "", 0);
 			}
 		}
 	}
@@ -28,7 +36,7 @@ public class Plot_Four extends Boardgame
 	protected void setUnits(int rows, int cols) 
 	{
 		Monad[][] discs = new Disc[rows][cols];
-		brigade = new Unit(discs);
+		brigade = new Unit_Manager(discs);
 		
 		for(int row = 0; row < rows; row++) 
 		{
@@ -56,5 +64,54 @@ public class Plot_Four extends Boardgame
 	@Override
 	public void play() 
 	{
+	}
+
+	@Override
+	protected boolean win(Position[][] grid, Position pos, int v, int u, int n) 
+	{
+		int r = pos.row, c = pos.col, count = 0;
+
+		//Checks half a vertical, diagonal, or horizontal line based on the arguments assigned to r and c
+		while ((r >= 0 && c >= 0) && (r < grid.length && c < grid[r].length)) {
+
+			if (grid[r][c].ruler == this)
+				count++;
+			else
+				break;
+
+			r += v;
+			c += u;
+		}
+		
+		//Re-assigns the values of y and x to r and c to check the other half of the line
+		r = pos.row; 
+		c = pos.col; 
+
+		while ((r >= 0 && c >= 0) && (r < grid.length && c < grid[r].length)) {
+
+			if (grid[r][c].ruler == this && (r != pos.row || c != pos.col))
+				count++;
+			else if (r != pos.row || c != pos.col)
+				break;
+
+			r += v * -1;
+			c += u * -1;
+
+		}
+
+		//Recursively checks the area centering a unit before returning the value of the unit
+		//that won or 0.
+		if (count == 4)
+			return true;
+		else if (n < 4) {
+			if (v + u == -1)
+				return win(grid, pos, v, 1, ++n);
+			else if (v + u == 0)
+				return win(grid, pos, 0, u, ++n);
+			else
+				return win(grid, pos, 1, u, ++n);
+		}
+		else
+			return false;
 	}
 }
