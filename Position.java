7f4ea75294e98467b4fe86fae0f586ce2583c326
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-12-28 00:17:17 -0800

    Update Position.java

--- Position.java
+++ Position.java
@@ -1,16 +1,29 @@
-package boardgame;
+package game;
 
 import javafx.scene.Node;
 import javafx.scene.image.*;
 import javafx.scene.shape.*;
 import javafx.scene.text.Text;
-import game.Monad;
+
+import game.Unit;
+import game.Player;
 
 public class Position extends Monad
 {
-	public Monad ruler;
-	public Position(Node icon, String name, double value) {
+	public Player[] players = null;
+	public Unit ruler = null;
+	
+	public Position(Player[] players, Unit ruler, Node icon, String name, double value) {
 		super(icon, name, value);
+		setPlayers(players);
+		this.ruler = ruler;
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void setPlayers(Player[] players) 
+	{
+		// TODO Auto-generated method stub
+		this.players = players;
+	}
+	
 }
