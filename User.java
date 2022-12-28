0f9c7482456951f17a4607a3355471fd37bc377e
Jaengus <116926495+Jaengus@users.noreply.github.com>
2022-12-28 00:19:13 -0800

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,17 @@
-package user;
+package game;
 
-public class User {
+import user.User;
+import game.Monad;
+import game.Unit;
+
+public class Player extends User
+{
+	private Monad [] monads = null;
+	private Unit [] units = null;
+
+	public Player(String name, String username) {
+		super(name, username);
+		// TODO Auto-generated constructor stub
+	}
 
 }
